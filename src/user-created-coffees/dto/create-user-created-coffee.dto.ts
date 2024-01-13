9040dea7ead8d35a63731ff7c2b238022ceedc95
idkimaya <112517716+idkimaya@users.noreply.github.com>
2024-01-13 20:57:50 +0100

    saving for mac update

---
+++ src/user-created-coffees/dto/create-user-created-coffee.dto.ts
@@ -0,0 +1,10 @@
+export class CreateUserCreatedCoffeeDto {
+    userID: number;
+    custom_coffee_title: string;
+    description: string;
+    size: string;
+    flavorID: number;
+    toppingID: number;
+    milkTypeID: number;
+  }
+  
---
+++ src/user-created-coffees/dto/update-user-created-coffee.dto.ts
@@ -0,0 +1,10 @@
+export class UpdateUserCreatedCoffeeDto {
+    userID?: number;
+    custom_coffee_title?: string;
+    description?: string;
+    size?: string;
+    flavorID?: number;
+    toppingID?: number;
+    milkTypeID?: number;
+  }
+  
